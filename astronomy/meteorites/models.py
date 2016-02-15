3c93e35953993d2eb75d753f47378aef89c562a4
dinesh <dineshpy07@gmail.com>
2016-02-15 23:37:54 +0530

    add api for all meteorites

--- astronomy/meteorites/models.py
+++ astronomy/meteorites/models.py
@@ -2,4 +2,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Meteorites(models.Model):
+    id = models.CharField(primary_key=True, max_length=50)
+    name = models.CharField(max_length=50)
+    nametype = models.CharField(max_length=50)
+    recclass = models.CharField(max_length=50)
+    mass = models.CharField(max_length=50)
+    fall = models.CharField(max_length=50)
+    #year = models.DateTimeField('year landed')
+    year = models.CharField(max_length=50)
+    reclat = models.CharField(max_length=50)
+    reclong = models.CharField(max_length=50)
+    geolocation_address = models.CharField(max_length=50)
+    geolocation_zip = models.CharField(max_length=50)
+    geolocation_city = models.CharField(max_length=50)
+    geolocation = models.CharField(max_length=50)
+    geolocation_state = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'meteorites'
--- astronomy/meteorites/views.py
+++ astronomy/meteorites/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from meteorites.models import Meteorites
+from meteorites.serializers import MeteoritesSerializer
+
+
+class MeteoriteViewSet(viewsets.ModelViewSet):
+    queryset = Meteorites.objects.all()
+    serializer_class = MeteoritesSerializer
